e7811250372aa7f97121712391e4b1fafefc8e07
Aniket Yadav <aniketyadavtech1@gmail.com>
2025-07-19 08:22:26 +0530

    pattern4

--- pattern/pattern4.java
+++ pattern/pattern4.java
@@ -1,5 +1,10 @@
    class  Pattern4{
     public static void main(String[] args) {
+// 1 
+// 2 2 
+// 3 3 3 
+// 4 4 4 4 
+// 5 5 5 5 5 
         int n =5;
         for(int i=1;i<=n;i++){
             for(int j =1;j<=i;j++){
