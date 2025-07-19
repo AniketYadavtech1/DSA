c4edb81a5fff9b1ab7f68106227cab096b37e4c3
Aniket Yadav <aniketyadavtech1@gmail.com>
2025-07-19 08:15:25 +0530

    pattern5

---
+++ pattern/pattern5.java
@@ -0,0 +1,22 @@
+public class pattern5 {
+    public static void main(String[] args) {
+// * * * * *
+// * * * * 
+// * * * 
+// * *  
+// * 
+// step 1 = loop 1 to n i--;
+// step 1 = loop 2 j<=i;
+// print (*);
+
+int n =5;
+for(int i =n;i<1;i--){
+    for(int j=n;j<1;j--){
+        System.out.print(" *");
+    }
+    System.out.println();
+    }
+
+   }
+   
+}
--- reverse_String1.java
+++ reverse_String1.java
@@ -3,11 +3,11 @@
     public static void main(String[] args) {
         String name = "Aniket";
         String homeName = "";
-        for(int i= name.length();i>0;i--){
-        homeName= homeName + charAt();
-        System.err.println(homeName);
+        for(int i= name.length()-1;i>=0;i--){
+       homeName = homeName + name.charAt(i);
+       
         }
-
+       System.err.println(homeName);
         
     }
     
