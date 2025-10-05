95fe819b91f42058839df5570a7bff138235299d
Aniket Yadav <aniketyadavtech1@gmail.com>
2025-10-05 23:47:53 +0530

    revArray

---
+++ array/list/reverse.dart
@@ -0,0 +1,22 @@
+void reverList() {
+List<int>  number = [1,2,3,4,5,];
+  int end = number.length-1;
+  int start = 0;
+while(start < end){
+int temp = number[start];
+number[start]=number[end];
+number[end]=temp;
+start++;
+  end--;
+
+for(int i =0;i<number.length;i++){
+  // print("${number[i]}");
+  
+}
+
+}
+
+}
+main() {
+  reverList();
+}
