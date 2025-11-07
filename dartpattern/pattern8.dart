68c5e939f1f4b23cf8a97233bc9179bd98c26563
Aniket Yadav <aniketyadavtech1@gmail.com>
2025-11-07 07:54:19 +0530

    add

--- dartpattern/pattern8.dart
+++ dartpattern/pattern8.dart
@@ -22,11 +22,26 @@ void pattern9(){
   }
 
 
+// 1                 1
+// 1 2             2 1
+// 1 2 3         3 2 1
+// 1 2 3 4     4 3 2 1
+// 1 2 3 4 5 5 4 3 2 1
+  
+
 
 
 
 }
 
+void numberPattern(){
+  int n =5;
+  for(int i=1;i>=n;i--){
+    print(i);
+  }
+}
+
 void main(){
-  pattern9();
+  // pattern9();
+  numberPattern();
 }
