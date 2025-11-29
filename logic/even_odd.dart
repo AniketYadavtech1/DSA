eb6d40d6a817637f2e2bb4aa39758d59cdf82b5b
Aniket Yadav <aniketyadavtech1@gmail.com>
2025-11-29 23:54:56 +0530

    main

---
+++ logic/even_odd.dart
@@ -0,0 +1,12 @@
+void evenOdd(){
+  int  n = 12;
+  if(n%2==0){
+    print("Even");
+  }else{
+    print("Odd");
+  }
+}
+
+void main(){
+  evenOdd();
+}
---
+++ logic/table.dart
@@ -0,0 +1,13 @@
+// print 2 table 
+
+
+void table(){
+  int n = 2;
+  for(int i =1;i<=10;i++){
+    print(n*i);
+  }
+}
+
+void main(){
+  table();
+}
