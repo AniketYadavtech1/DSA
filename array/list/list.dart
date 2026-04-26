8fca4276844f493595287a70a7011944b63adb69
Aniket Yadav <aniketyadavtech1@gmail.com>
2026-04-26 18:34:04 +0530

    welcome

--- array/list/list.dart
+++ array/list/list.dart
@@ -1,10 +1,54 @@
-listcomplete(){
-  List<int> number = [1,2,3,4,5,6];
- int n =  number.length;
+listcomplete() {
+  List<int> number = [1, 2, 3, 4, 5, 6];
+  int n = number.length;
   print(number);
 }
-void main(){
-  listcomplete();
 
+void dublicate() {
+  List<int> product = [1, 2, 3, 5, 3, 10, 4];
+  // step 1 first start loop from 1 to product.length;
+  // step 2 secon loop i+1 to i<=product.length;
+  // step 3 if(product[i]==product[j]) print product[i]
+  // int dublicate = 0;
+  // for (int i = 0; i <= product.length; i++) {
+  //   for (int j = i + 1; j <= product.length - 1; j++) {
+  //     if (product[i] == product[j]) {
+  //       print(product[i]);
+  //       break;
+  //     }
+  //   }
+  // }
 
+  // it is dart is fast and effective programming
+  // Set<int>seen = {};
+  // Set<int>dublicates = {};
+
+  // for(int num in product){
+  //   if(seen.contains(num)){
+  //     dublicates.add(num);
+  //   }else{
+  //     seen.add(num);
+  //   }
+  //   }
+  //   print(dublicates);
+
+  // }
+
+  // max number logic
+
+  int max=0;
+
+  // logic int max for 0 to n;
+  // step while max<=0;
+  for (int i = 0; i <= product.length-1;i++) {
+    if (max >=product[i]) {
+      max = product[i];
+      print(max);
+    }
+  }
+}
+
+void main() {
+  dublicate();
+  // listcomplete();
 }
